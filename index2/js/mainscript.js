bbd86a6483be8d0e36ff1b07fd068a33d1638a3a
juned.khan <juned3you@gmail.com>
2014-12-03 11:37:21 +0530

    Initial site

---
+++ index2/js/mainscript.js
@@ -0,0 +1,54 @@
+// JavaScript Document
+
+$(document).ready(function(e){
+	
+	//BANNER
+	$('.cc_banner ul').bxSlider(
+		{
+		
+		auto:true,
+		pager:false,
+		controls:true,
+		nextText:' ',
+		prevText:' ' 
+		}
+	);
+	
+	//small slider
+	$('.cc_slider ul').bxSlider(
+		{
+		mode:'fade',
+		speed:1500,
+		auto:true,
+		pager:false,
+		controls:true,
+		nextText:' ',
+		prevText:' ' 
+		}
+	);
+	
+	
+	//ftrd product slider
+	$(".cc_carousel ul ").carouFredSel({
+			width   : "100%",
+			scroll  : {
+				items :1 ,
+				pauseOnHover: true
+				},
+			prev	: {	
+				button	: "#cc_prev",
+				key		: "left"
+			},
+			next	: { 
+				button	: "#cc_next",
+				key		: "right"
+			},
+			circular: true,
+			infinite: false,
+			auto 	: true,
+	});
+	
+	
+	
+	
+});
